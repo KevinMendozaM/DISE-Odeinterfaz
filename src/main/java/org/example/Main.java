d5e4a65eb18230d46fa3439137b956902edb8d6e
POO <wilmer.vargas@epn.edu.ec>
2024-06-20 09:56:43 -0500

    km5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,20 @@
 package org.example;
 
+import javax.swing.*;
+
 public class Main {
     public static void main(String[] args) {
+
+
+        String primernumero = JOptionPane.showInputDialog("Introduzca el primer numero");
+        String segundonumero = JOptionPane.showInputDialog("Introduzca el segundo numero");
+
+        int numero = Integer.parseInt(primernumero);
+        int numero2 = Integer.parseInt(segundonumero);
+        int resultado = numero + numero2;
+
+        JOptionPane.showMessageDialog(null, "El resultado es: " + resultado, "suma", JOptionPane.INFORMATION_MESSAGE);
+
 
     }
 }
